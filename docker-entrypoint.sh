79c203a53ba1a64b0e606fc1cbf62a6e1999b9f4
seshurajramesh <seshuraj1998@gmail.com>
2026-01-03 22:43:55 +0530

    This app has got the changes to run with docker compose with Postgres on container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
+# Optional: Wait for postgres to be ready
+echo "Waiting for postgres..."
+sleep 3 
+
+# Initialize and run migrations
+flask db init || true  # '|| true' ignores error if already initialized
+flask db migrate -m "Initial migration"
 flask db upgrade
+
+# Start the Flask app
 
 exec gunicorn --bind 0.0.0.0:80 "app:create_app()"
